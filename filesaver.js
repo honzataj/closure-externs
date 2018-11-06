52ac5afb2ac01f8225ee96ab4f429c092643050f
Jan Tajovský <423920@muni.cz>
2018-11-06 09:10:32 +0100

    Renamed new JS files

---
+++ filesaver.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview Externs for fileSaver.
+ * @externs
+ */
+
+
+/**
+ * @param {Blob} blob
+ * @param {string} name
+ * @param {Object=} opt_options
+ */
+var saveAs = function (blob, name, opt_options) {};
